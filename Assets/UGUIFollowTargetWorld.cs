073b8b72ffad0032b2a3f9474aaa7d5ee320122f
hsienwei <hsienwei@gmail.com>
2018-02-27 16:26:47 +0800

    增加world版本.

---
+++ Assets/UGUIFollowTargetWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UGUIFollowTargetWorld : MonoBehaviour
+{
+  public Camera m_CurCamera;
+  public Transform m_Target;
+
+
+  //private RectTransform m_SelfRectTransform;
+
+  void Start()
+  {
+    //m_SelfRectTransform = this.GetComponent<RectTransform>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    transform.position = m_Target.position;// RectTransformUtility.WorldToScreenPoint(m_CurCamera, m_Target.position);
+
+  }
+}
--- Assets/UIMoveTestMain.cs
+++ Assets/UIMoveTestMain.cs
@@ -14,8 +14,16 @@ public class UIMoveTestMain : MonoBehaviour {
   public UI2DSprite OriImage2;
   public Camera UICamera2;
 
+  public Canvas UIRoot3;
+  public UnityEngine.UI.Image OriImage3;
+
+  public GameObject UIRoot4;
+  public UIPanel OriImage4;
+
   public GameObject Switch1;
   public GameObject Switch2;
+  public GameObject Switch3;
+  public GameObject Switch4;
 
   // Use this for initialization
   void Start () {
@@ -30,28 +38,68 @@ public class UIMoveTestMain : MonoBehaviour {
       ac.m_CurCamera = m_Camera;
 
       var b = NGUITools.AddChild(UIRoot2, OriImage2.gameObject);
-
       var bc = b.gameObject.AddComponent<NGUIFollowTarget>();
       bc.m_Target = Obj.transform;
       bc.m_CurCamera = m_Camera;
       bc.m_UICamera = UICamera2;
+
+      var c = GameObject.Instantiate(OriImage3);
+      c.rectTransform.parent = UIRoot3.transform;
+      var cc = c.gameObject.AddComponent<UGUIFollowTargetWorld>();
+      cc.m_Target = Obj.transform;
+      cc.m_CurCamera = m_Camera;
+
+      var d = NGUITools.AddChild(UIRoot4, OriImage4.gameObject);
+      var dc = d.gameObject.AddComponent<UGUIFollowTargetWorld>();
+      dc.m_Target = Obj.transform;
+      dc.m_CurCamera = m_Camera;
     }
-	}
+
+    Switch1.SetActive(true);
+    Switch2.SetActive(false);
+    Switch3.SetActive(false);
+    Switch4.SetActive(false);
+  }
 
   void OnGUI()
   {
-    if(GUI.Button(new Rect(100, 100, 100, 100), "Switch"))
+    if (Switch1.active)
+      GUI.Label(new Rect(100, 100, 100, 50), "UGUI UI");
+    if (Switch2.active)
+      GUI.Label(new Rect(100, 100, 100, 50), "NGUI UI");
+    if (Switch3.active)
+      GUI.Label(new Rect(100, 100, 100, 50), "UGUI World");
+    if (Switch4.active)
+      GUI.Label(new Rect(100, 100, 100, 50), "NGUI World");
+
+    if (GUI.Button(new Rect(200, 100, 100, 50), "UGUI UI"))
     {
-      if (Switch1.active)
-      {
-        Switch1.SetActive(false);
-        Switch2.SetActive(true);
-      }
-      else
-      {
-        Switch1.SetActive(true);
-        Switch2.SetActive(false);
-      }
+      Switch1.SetActive(true);
+      Switch2.SetActive(false);
+      Switch3.SetActive(false);
+      Switch4.SetActive(false);
     }
+    if (GUI.Button(new Rect(300, 100, 100, 50), "NGUI UI"))
+    {
+      Switch1.SetActive(false);
+      Switch2.SetActive(true);
+      Switch3.SetActive(false);
+      Switch4.SetActive(false);
+    }
+    if (GUI.Button(new Rect(400, 100, 100, 50), "UGUI World"))
+    {
+      Switch1.SetActive(false);
+      Switch2.SetActive(false);
+      Switch3.SetActive(true);
+      Switch4.SetActive(false);
+    }
+    if (GUI.Button(new Rect(500, 100, 100, 50), "NGUI World"))
+    {
+      Switch1.SetActive(false);
+      Switch2.SetActive(false);
+      Switch3.SetActive(false);
+      Switch4.SetActive(true);
+    }
+
   }
 }
